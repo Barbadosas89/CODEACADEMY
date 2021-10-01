00d8714581f5c837e771f7f578266ae94d5bb72a
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-10-01 13:58:51 +0300

    01/10/2021 - Strings

---
+++ CodeAcademy/classworks/Strings/script.js
@@ -0,0 +1,4 @@
+let userInfoOutput = document.getElementById("main");
+let user = {name: "Donatas", surname: "Balionis" };
+document.getElementById("main").innerHTML =  `<h2>User Information</h2>
+<p>User name is ${user.name}, and surname is ${user.surname}</p>`;
