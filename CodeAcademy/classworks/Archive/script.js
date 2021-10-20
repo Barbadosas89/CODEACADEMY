86059cef34e8f656ff0f699d85067925d2943e07
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-10-20 18:28:51 +0300

    Update script.js

--- CodeAcademy/classworks/Archive/script.js
+++ CodeAcademy/classworks/Archive/script.js
@@ -2,8 +2,8 @@
 let numbers = [5, 1, 7, 2, -9, 8, 2,  7, 9, 4, -5, 2, -6, -4, 6];
 // // 1. Parašykite funkciją arrDoubled, kuri sukuria ir grąžina naują masyvą, kurio elementai padauginti iš 2;
 
-let arrDouble = num =>numbers.map(num => num * 2);
-console.log(arrDouble(numbers));
+let arrDouble = (num, numb) =>numbers.map(num => num * numb);
+console.log(arrDouble(numbers,2));
 //
 //
 // // 2. Parašykite funkciją arrMultiplied, kuri sukuria ir grąžina naują masyvą, kurio elementai padauginti iš argumentu nurodyto skaičiaus
