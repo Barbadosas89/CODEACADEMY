cade2d50fb7efb32c72fb6e1f687db79782ace3b
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-10-20 21:53:40 +0300

    Add files via upload

---
+++ CodeAcademy/classworks/DOM/app.js
@@ -0,0 +1,6 @@
+
+console.log(document.querySelector('#age-table'))
+console.log(document.querySelector('#age'))
+console.log(document.querySelectorAll('label'))
+console.log(document.querySelector('input'))
+console.log(document.querySelectorAll('input:last-of-type'))
