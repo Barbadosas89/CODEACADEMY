009b1d7c455ce517f1d1b7365d72c8d6403d565d
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-11-03 20:34:05 +0200

    Add files via upload

--- CodeAcademy/classworks/3.11.2021/OOP/assets/app.js
+++ CodeAcademy/classworks/3.11.2021/OOP/assets/app.js
@@ -1,2 +1,4 @@
 let movie1 = new Movie('Avengers: Endgame', 2019, 'Marvel', 400000000, 2797800564);
 let movie2 = new Movie('Justice League', 2017, 'Marvel', 300000000, 655945209);
+console.log(movie1.getIntroduction(), movie1.getProfit());
+console.log(movie2.getIntroduction(), movie2.getProfit());
