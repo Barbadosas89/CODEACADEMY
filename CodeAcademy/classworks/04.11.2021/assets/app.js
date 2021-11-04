8cfec9f7880e9317b15a3cbe5ca22f049e7449df
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-11-04 20:05:41 +0200

    Update app.js
    adding moto parameters

--- CodeAcademy/classworks/04.11.2021/assets/app.js
+++ CodeAcademy/classworks/04.11.2021/assets/app.js
@@ -7,5 +7,5 @@ console.log(car2.getIntroduction(), car2.getAge(), car2.currentCarAge());
 console.log(car)
 console.log(car2)
 
-let moto = new Motorcycle(5);
+let moto = new Motorcycle('Honda', 'CBR', 2021, 5);
 console.log(moto.getWheelsNumber())
