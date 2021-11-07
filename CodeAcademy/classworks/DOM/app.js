c52aa48424d0db2d6c5bcd95b68c50542da60ba2
Barbadosas89 <barbadosas89@outlook.com>
2021-11-07 20:35:57 +0200

    slamstas

--- CodeAcademy/classworks/DOM/app.js
+++
@@ -1,6 +0,0 @@
-
-console.log(document.querySelector('#age-table'))
-console.log(document.querySelector('#age'))
-console.log(document.querySelectorAll('label'))
-console.log(document.querySelector('input'))
-console.log(document.querySelectorAll('input:last-of-type'))
