4fd0d37566909966350f467f312d27f2e6c94d2c
Barbadosas89 <barbadosas89@outlook.com>
2021-11-09 21:41:45 +0200

    fetch

---
+++ CodeAcademy/classworks/09.11.2021/assets/app.js
@@ -0,0 +1,26 @@
+let table = document.createElement('table');
+table.setAttribute('id', 'data')
+document.body.appendChild(table)
+
+
+
+
+fetch("https://jsonplaceholder.typicode.com/posts").then(
+    res => {
+        res.json().then(
+            data => {
+                console.log(data);
+                if (data.length > 0) {
+
+                    let temp = "";
+                    data.forEach((title) => {
+                        temp += "<tr>";
+                        temp += "<td>" + title.title + "</td></tr>";
+                    });
+                    document.getElementById('data').innerHTML = temp;
+                    document.body.appendChild(table)
+                }
+            }
+        )
+    }
+)
