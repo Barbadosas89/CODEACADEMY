4bb29bf95de8f353528db40729961c5f4352ecbd
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-12-01 19:51:05 +0200

    Update index3.php

--- 2021-12-01/index3.php
+++ 2021-12-01/index3.php
@@ -2,6 +2,6 @@
 
 // Sukurkite masyvą $arr iš reikšmių: '!', 'Hello', 'world'. Naudodami sukurtą masyvą priskirti reikšme 'Hello, world!'
 //  naujam kintamajam ir atspausdinkite jį ekrane.
-$array=['!', 'Hello', 'world'];
-$new_arr="'$array[1], $array[2] $array[0]'.";
+$arr=['!', 'Hello', 'world'];
+$new_arr="'$arr[1], $arr[2] $arr[0]'.";
 echo $new_arr;
