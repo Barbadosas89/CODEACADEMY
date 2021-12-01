261951d8917e44c493cc7e92115a34fe5e170fad
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-12-01 20:31:55 +0200

    Add files via upload

--- 2021-12-01/index4.php
+++ 2021-12-01/index4.php
@@ -16,18 +16,18 @@ for ($i=1; $i<=9; $i++) {
     for ($k=0; $k<$i; $k++)
     
     echo($i);   
-        echo('<br>');    
+        echo("<br>");    
     }
     for($i=1;$i<=9;$i++)
     {
         echo str_repeat($i, $i);
-        echo "<br />";
+        echo "<br>";
     }
     
-    echo "<br />";
+    echo "<br>";
     
     for($i=9;$i>=1;$i--)
     {
         echo str_repeat($i, $i);
-        echo "<br />";
+        echo "<br>";
     }
