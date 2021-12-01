3d6f60f93805007d118422d62028d941aba09a24
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-12-01 20:28:22 +0200

    Add files via upload

--- 2021-12-01/index3.php
+++ 2021-12-01/index3.php
@@ -3,5 +3,9 @@
 // Sukurkite masyvą $arr iš reikšmių: '!', 'Hello', 'world'. Naudodami sukurtą masyvą priskirti reikšme 'Hello, world!'
 //  naujam kintamajam ir atspausdinkite jį ekrane.
 $arr=['!', 'Hello', 'world'];
-$new_arr="'$arr[1], $arr[2] $arr[0]'.";
+$new_arr="'$arry[1], $arr[2] $arr[0]'.";
 echo $new_arr;
+
+$vowels = array("d", "n", "t", "o", "u", "A", "E", "O", "U");
+$onlyconsonants = str_replace($vowels, "", "I dont like PHP");
+echo $onlyconsonants;
