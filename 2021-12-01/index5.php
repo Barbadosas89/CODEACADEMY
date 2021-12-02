325e1a513883b522f6285a7d4c3f09e7a6ccde36
Donatas <90237321+Barbadosas89@users.noreply.github.com>
2021-12-02 20:10:21 +0200

    Update index5.php

--- 2021-12-01/index5.php
+++ 2021-12-01/index5.php
@@ -1,4 +1,3 @@
-
 <?php
 
 // Sukurkite dvimatį masyvą. Pirmieji du raktai yra lt ir en.
@@ -6,8 +5,8 @@
 // Naudodamiesi šiuo masyvu, pirmadienį parodykite lietuvių kalba, o trečiadienį - anglų kalba.
 // Sukurkite kintamuosius lang (reikšmės lt arba en) ir parodykite dieną
 $days = [
-    'lt' => ["pirmadienis", "antradienis", "trečiadienis", "ketvirtadienis", "penktadienis", "šeštadienis","sekmadienis"],
-    'en' => ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday","sunday"],
+    'lt' => ["Pirmadienis", "Antradienis", "Trečiadienis", "Ketvirtadienis", "Penktadienis", "Šeštadienis","Sekmadienis"],
+    'en' => ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"],
 
 ];
 echo $days['lt'][0], '<br>', $days['en'][2], '<br>';
@@ -16,14 +15,13 @@ echo $days['lt'][0], '<br>', $days['en'][2], '<br>';
 
 $lang = 'en';
 
-$date = date('l');
-$today = date('w');
+$todayDay = date('l');
+$weekDays = date('w');
 
 if ($lang === 'en') {
-    $return = "Today is $date";
+    $return = "Today is $todayDay";
 } else {
-    $result = $days[$lang][$today-1];
-    $return = "Siandien yra $result";
+    $result = $days[$lang][$weekDays-1];
+    $return = "Šiandien yra $result";
 }
 echo $return;
-
